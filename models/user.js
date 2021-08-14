5ca4cb56c382ec201cf65a239f2686a02b5cc2fd
Shrinivas Kenjale <sskenjale22@protonmail.ch>
2021-08-14 21:47:43 +0530

    change default user about

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 export const userSchema = new Schema({
   about: {
     type: String,
-    default: "Still working on it!",
+    default: "Hey there! I am using Sweeter.",
     required: true,
   },
 });
